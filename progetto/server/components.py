6b20e0773896ca78fdb209198295eeba0abeab95
Antonjio <147497461+Antonjio@users.noreply.github.com>
2023-12-22 15:25:19 +0100

    Delete progetto directory

--- progetto/server/components.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-components = Blueprint('components', __name__)
-
-
-@components.route('/navbar')
-def navbar():
-  """ route to get navbar component """
-  return render_template('navbar.html')
--- progetto/server/sw.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, send_from_directory
-
-sw = Blueprint('sw', __name__)
-
-
-@sw.route('/sw')
-def get_service_worker():
-  return send_from_directory('../static', 'sw.js')
